ae089258694c0d8ea85903e061ec33ec158da82c
Kevin Harrington <mad.hephaestus@gmail.com>
2021-06-22 15:29:23 -0400

    sending card data over HID

--- CardReadCommand.cpp
+++ CardReadCommand.cpp
@@ -6,7 +6,7 @@
  */
 
 #include "CardReadCommand.h"
-
+#include <Arduino.h>
 CardReadCommand::CardReadCommand() :
 PacketEventAbstract(44) {
 
@@ -17,6 +17,11 @@ CardReadCommand::~CardReadCommand() {
 }
 
 void CardReadCommand::event(float * buffer){
+	if(recentCard>0){
+		Serial.println("processing card "+String(recentCard));
+	}
 	buffer[0]=recentCard;
+	buffer[1]=-2;
+	buffer[2]=-3;
 	recentCard=-1;
 }
